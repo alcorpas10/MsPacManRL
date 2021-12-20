0433e4a7b0d7adbab9a87a65893907a69c77a0c5
alcorpas10 <alcorpas@ucm.es>
2021-12-20 12:09:14 +0100

    Update GeneralTransition.java

--- src/main/java/FSM/GeneralTransition.java
+++ src/main/java/FSM/GeneralTransition.java
@@ -4,17 +4,17 @@ import es.ucm.fdi.ici.Input;
 
 public class GeneralTransition implements Transition {
 	String id;
+	
 	public GeneralTransition(String nombre) {
-		// TODO Auto-generated constructor stub
-		super();
 		id=nombre;
 	}
+	
 	@Override
 	public boolean evaluate(Input in) {
-		// TODO Auto-generated method stub
-		MsPacManInput input = (MsPacManInput)in;	
+		MsPacManInput input = (MsPacManInput) in;	
 		return input.general();
 	}
+	
 	public String toString() {
 		return id + " general";
 	}
