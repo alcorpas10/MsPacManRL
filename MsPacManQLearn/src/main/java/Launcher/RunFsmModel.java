e365a5c169309ac662447bbf0eba6d72fa5a74fa
Dbugoi <60663710+Dbugoi@users.noreply.github.com>
2022-05-25 17:24:21 +0200

    Cambio nombre clase por claridad qlearn

---
+++ MsPacManQLearn/src/main/java/Launcher/RunFsmModel.java
@@ -0,0 +1,24 @@
+package Launcher;
+
+import Others.Executor;
+import Others.GhostRandom;
+import pacman.controllers.GhostController;
+
+/**
+ * Class that runs the game with a fsm 
+ */
+public class RunFsmModel {
+	public static void main(String[] args) {
+		//Class that executes the game
+		Executor executor = new Executor.Builder()
+				.setTickLimit(4000)
+				.setVisual(true)
+				.setScaleFactor(3.0)
+				.build();
+		
+		GhostController ghosts = new GhostRandom(); 	//The ghosts that we want to play against
+		
+
+		System.out.println(executor.runGameFSM(ghosts, 40)); //Runs and shows the game executing the FSM
+	} 
+}
